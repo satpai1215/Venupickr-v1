a4bacf02ae06e63710e82db026dbc4fb3228a9f4
satpai1215 <pai.satyan@gmail.com>
2013-04-02 22:46:31 -0700

    migration remove_date_from_events corrected for heroku

--- db/migrate/20130305040337_remove_date_from_events.rb
+++ db/migrate/20130305040337_remove_date_from_events.rb
@@ -2,7 +2,6 @@ class RemoveDateFromEvents < ActiveRecord::Migration
   def change
   	add_column :events, :event_start, :datetime
   	add_column :events, :vote_start, :datetime
-  	add_column :events, :vote_start_date, :date
   	rename_column :events, :time, :event_start_time
   	add_column :events, :event_start_date, :date
   end
