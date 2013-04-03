c133bbd6dc8a59168bf23b2c57c203f6a15a740b
satpai1215 <pai.satyan@gmail.com>
2013-04-02 23:03:18 -0700

    migration file fixed

--- db/migrate/20130307044830_change_vote_date_to_integer.rb
+++ db/migrate/20130307044830_change_vote_date_to_integer.rb
@@ -1,11 +1,11 @@
 class ChangeVoteDateToInteger < ActiveRecord::Migration
   def up
-  	remove_column :events, :vote_start, :datetime
+  	remove_column :events, :vote_start
   	add_column :events, :vote_start, :integer
   end
 
   def down
-  	remove_column :events, :vote_start, :integer
+  	remove_column :events, :vote_start
   	add_column :events, :vote_start, :datetime
   end
 end
