d7934bc695c4f3b0fdb7a37089ef3d946bc09266
satpai1215 <pai.satyan@gmail.com>
2013-04-02 23:00:14 -0700

    migration RemoveDateFromEvents changed

--- db/migrate/20130305040337_remove_date_from_events.rb
+++ db/migrate/20130305040337_remove_date_from_events.rb
@@ -1,10 +1,5 @@
 class RemoveDateFromEvents < ActiveRecord::Migration
-  def change
-  	add_column :events, :event_start, :datetime
-  	add_column :events, :vote_start, :datetime
-  	rename_column :events, :time, :event_start_time
-  	add_column :events, :event_start_date, :date
-  end
+
   def up
   	remove_column :events, :date
   end
