0fb010f13672662e494ada542ced2afd693fa307
satpai1215 <pai.satyan@gmail.com>
2013-04-23 23:37:45 -0700

    mailer configured

--- app/mailers/auto_mailer.rb
+++ app/mailers/auto_mailer.rb
@@ -6,7 +6,7 @@ class AutoMailer < ActionMailer::Base
   	@url = event_url(@event, :host => "0.0.0.0:3000")
   	mail(:to => "pai.satyan@gmail.com", :subject => "Your Event Has Finished")
   end
-  handle_asynchronously :event_email
+  #handle_asynchronously :event_email
 
   def vote_email(event_id)
   	@event = Event.find(event_id)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,19 @@ MoMondays::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  # Mailer options
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  #:domain               => 'baci.lindsaar.net',
+  :user_name            => 'pai.satyan@gmail.com',
+  :password             => 'Fru!t4521',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -36,5 +47,4 @@ MoMondays::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
