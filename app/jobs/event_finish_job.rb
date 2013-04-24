86297093deef7d326fc2cf96f9812d0da91536fe
satpai1215 <pai.satyan@gmail.com>
2013-04-23 21:59:28 -0700

    tiebreaker sorted by created_at

--- app/jobs/event_finish_job.rb
+++ app/jobs/event_finish_job.rb
@@ -3,7 +3,8 @@ class EventFinishJob < Struct.new(:event_id)
 	def perform
 		@event = Event.find(event_id)
     	@event.update_attributes(:stage => "Finished")
-   	 	@event.update_attributes(:winner => (@event.venues.count != 0 ? @event.venues.order("votecount DESC").first.id : nil))
+    	#ties are broken by first venue created
+   	 	@event.update_attributes(:winner => (@event.venues.count != 0 ? @event.venues.order("votecount DESC, created_at ASC").first.id : nil))
     	#Automailer.event_email(event_id).deliver
 	end
 
