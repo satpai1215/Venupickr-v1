161f8ee5d66f5054b85f908a9806ed7c5806648c
satpai1215 <pai.satyan@gmail.com>
2013-04-25 17:13:55 -0700

    changed production host to herokuapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ MoMondays::Application.configure do
   config.assets.digest = true
 
     # Mailer options
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'momondays.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "pai.satyan@gmail.com"
+  config.mailer_sender = "momondaysmailer@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
