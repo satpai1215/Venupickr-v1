2354c8d54a15dfef85a541991ec39459ca73d08c
satpai1215 <pai.satyan@gmail.com>
2013-05-13 17:33:27 -0700

    Update cleaning in update.rb

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,11 +1,4 @@
 class UpdatesController < ApplicationController
 
-	#cleans out Update log after 50 entries
-	def create
-	    if Update.count > 20
-	    	batch = Update.limit(15).order('created_at asc')
-	    	batch.destroy_all
-	    end
-  	end
 
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -3,4 +3,16 @@ class Update < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
 
+  after_create :clean_update_log
+
+
+  private
+  #cleans out Update log after 50 entries
+  	def clean_update_log
+  		if ::Update.count > 50
+	    	batch = ::Update.limit(25).order('created_at ASC')
+	    	batch.destroy_all
+	    end
+  	end
+
 end
