1223ff7e83657e6b3609d97f273e4d2407bac228
satpai1215 <pai.satyan@gmail.com>
2013-05-13 17:47:12 -0700

    add_notification_email migration fix

--- db/migrate/20130513053240_add_notification_emails_to_users.rb
+++ db/migrate/20130513053240_add_notification_emails_to_users.rb
@@ -1,5 +1,5 @@
 class AddNotificationEmailsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :notification_emails, :boolean, :default => 1
+    add_column :users, :notification_emails, :boolean, :default => true
   end
 end
