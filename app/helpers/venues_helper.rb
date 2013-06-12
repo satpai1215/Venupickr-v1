9e256488d5d13cc22542c65039d283b6f5cf0ce2
satpai1215 <pai.satyan@gmail.com>
2013-06-11 22:17:12 -0700

    allow event owner to remove any venue

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,12 +1,15 @@
 module VenuesHelper
 	def display_modifiers_venue(venue)
-		html = ""
+		html = "<span class = 'venue_modifiers'>"
 		if(current_user == venue.user )
-			html = "<span class = 'venue_modifiers'>("
-			html<< link_to('Edit', edit_venue_path(venue), data: {confirm: 'Are you sure? Editing the venue will reset the vote count to zero'}, :remote => true, :class => 'edit-venue') + " / "
-	        html << link_to('Remove', venue, method: :delete, data:{ confirm: 'Are you sure?' }, :remote => true, :class => 'delete-venue')
-    		html << ")</span>"
+			html<< "(" + link_to('Edit', edit_venue_path(venue), data: {confirm: 'Are you sure? Editing the venue will reset the vote count to zero'}, :remote => true, :class => 'edit-venue') + ") / "
+	    end
+	    if(current_user == venue.event.user or current_user == venue.user)
+	        html << "(" + link_to('Remove', venue, method: :delete, data:{ confirm: 'Are you sure?' }, :remote => true, :class => 'delete-venue') + ")"
     	end
+
+    	html << "</span>"
+
     	return html.html_safe
 	end
 end
