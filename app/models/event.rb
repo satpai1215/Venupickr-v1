7a60f6fa5c78edbe79697658ef9931c443c2ba9f
satpai1215 <pai.satyan@gmail.com>
2013-06-14 08:39:45 -0700

    timepicker width

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,14 @@
 class Event < ActiveRecord::Base
   attr_accessible :name, :user_id, :stage, :event_start, :vote_end, :winner, :event_email_job_id,
-                  :voting_email_job_id, :notes
+                  :voting_email_job_id, :notes, :datepicker, :timepicker
   belongs_to :user
   has_many :venues, dependent: :destroy
   has_many :voters, dependent: :destroy
   has_many :rsvps, dependent: :destroy
 
   validates :name, presence: true
+  validates :datepicker, presence: true, :on => :create
+  validates :timepicker, presence: true, :on => :create
   #validate :event_start_valid?, :on => :create
 
   def to_s
