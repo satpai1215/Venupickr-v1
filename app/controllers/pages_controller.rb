e25abc8e9a9ce1c2b3f2e28f1890c471181e70b1
satpai1215 <pai.satyan@gmail.com>
2013-06-18 23:41:42 -0700

    vote_end autodisplay in event Form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
   end
 
   def history
-  	@events = Event.where(:stage => "Finished").order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+  	@events = Event.where(:stage => "Finished").order("event_start DESC").paginate(:page => params[:page], :per_page => 12)
   end
 end
