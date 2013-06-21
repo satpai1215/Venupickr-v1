a80ffd29ad7fea6863273e66981ffa5ea9aaf972
satpai1215 <pai.satyan@gmail.com>
2013-06-20 21:23:06 -0700

    sign_in for event#show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 
 class EventsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index]
 
 
   # GET /events
