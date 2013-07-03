60cdb6f63e2e5a42feb8c8f0f3a86f69d15f7ae6
satpai1215 <pai.satyan@gmail.com>
2013-07-02 18:50:05 -0700

    removed migration

--- db/migrate/20130317234410_add_venue_id_to_voters.rb
+++
@@ -1,9 +0,0 @@
-class AddVenueIdToVoters < ActiveRecord::Migration
-  def up
-  	add_column :voters, :venue_id, :integer
-  end
-
-  def down
-  	remove_column :voters, :venue_id, :integer
-  end
-end
