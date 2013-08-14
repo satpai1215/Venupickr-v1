62ffcc2849ad0d10c0ea706662001e89cab62b42
satpai1215 <pai.satyan@gmail.com>
2013-08-13 22:58:33 -0700

    leave_event auths

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,7 @@
 class GuestsController < ApplicationController
 	before_filter :authenticate_user!
 	before_filter :get_event, :only => [:new, :edit, :destroy]
-	before_filter :auth_owner, :only => [:new, :edit, :destroy]
+	before_filter :auth_owner, :only => [:edit, :destroy]
 
 #before_filter methods
 	def get_event
@@ -12,15 +12,18 @@ class GuestsController < ApplicationController
 		if(current_user.id != @event.owner_id)
 			redirect_to events_path, :notice => "You are not authorized to do that."
 		end
-
 	end
 #end before_filter methods
 
 	def new
-	    respond_to do |format|
-        	format.html { render :partial => "guest_form" }
-        	format.js
-	    end
+		if(current_user.id === @event.owner_id or true)
+		    respond_to do |format|
+	        	format.html { render :partial => "guest_form" }
+	        	format.js
+		    end
+		else
+			redirect_to events_path, :notice => "You are not authorized to do that."
+		end
 	end
 
 	def update_guestlist
@@ -43,9 +46,14 @@ class GuestsController < ApplicationController
 	end
 
 	def leave_event
-		@event = Event.find(params[:event_id])
-		@event.uninvite!(current_user.id)
-		redirect_to events_path, notice: "You successfully left '#{@event.name}.'"
+		#don't allow event_owner to leave their own event (they should delete the event)
+		if(current_user.id != @event.owner_id)
+			@event = Event.find(params[:event_id])
+			@event.uninvite!(current_user.id)
+			redirect_to events_path, notice: "You successfully left '#{@event.name}.'"
+		else
+			redirect_to events_path, :notice => "You cannot leave your own event. Please delete the event."
+		end
 	end
 
 
