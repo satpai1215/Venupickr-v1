7c3da085faa03a3bb378054796ddec68f7796a45
satpai1215 <pai.satyan@gmail.com>
2013-08-20 15:51:34 -0700

    vote meter update for owner working, no voteSort until refresh

--- app/assets/javascripts/runner.js
+++ app/assets/javascripts/runner.js
@@ -9,9 +9,9 @@ function voteSort() {
 
     $list.sort(function(a, b) {
         var aVotes = $(a).find(".voteCount").data("votecount");
-       // console.log("A:" + $(a).find(".voteCount").text());
+        console.log("A:" + aVotes);
         var bVotes = $(b).find(".voteCount").data("votecount");
-       // console.log("B:" + $(b).find(".voteCount").text());
+        console.log("B:" + bVotes);
 
         return parseInt(bVotes) - parseInt(aVotes);
     });
