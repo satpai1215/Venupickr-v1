059c90d1df396c1c9cdd66a584aa70863f77292d
satpai1215 <pai.satyan@gmail.com>
2013-10-30 22:00:24 -0700

    revert emails

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MoMondays::Application.initialize!
 
-#Mailer settings
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtpout.secureserver.net",
-  :port                 => 80,
-  :domain               => 'mo-mondays.com',
-  :user_name            => 'info@mo-mondays.com',
-  :password             => 'moproblems',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
-
-
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ MoMondays::Application.configure do
   # Mailer options
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
