4d7753511b9a482957ddc75cca4ec5f9255cd5c8
satpai1215 <pai.satyan@gmail.com>
2013-10-30 21:50:53 -0700

    action mailer to env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MoMondays::Application.initialize!
 
-
+#Mailer settings
+ config.action_mailer.smtp_settings = {
+  :address              => "smtpout.secureserver.net",
+  :port                 => 80,
+  :domain               => 'mo-mondays.com',
+  :user_name            => 'info@mo-mondays.com',
+  :password             => 'moproblems',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ MoMondays::Application.configure do
     # Mailer options
   config.action_mailer.default_url_options = { :host => 'mo-mondays.com' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => "smtpout.secureserver.net",
