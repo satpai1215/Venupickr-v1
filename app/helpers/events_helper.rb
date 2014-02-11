21cccf28fc41e6630ab667387de459ad37872f41
satpai1215 <pai.satyan@gmail.com>
2014-02-10 22:38:13 -0800

    event show styling

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -100,7 +100,7 @@ module EventsHelper
 		if (event.stage == 'Voting')
 			if(event.allow_venue_suggestion or current_user.id === event.owner_id)
 				html << "#{link_to 'Suggest a Venue', 
-				new_event_venue_path(event), {:class => 'newVenueBtn btn btn-info', :remote => true}}"
+				new_event_venue_path(event), {id: 'newVenueBtn', class: 'btn btn-info', :remote => true}}"
 			else
 				html <<"<span id = 'event_show_venue_suggestion_off_tag'>venue suggestion has been turned OFF</span>"
 			end
