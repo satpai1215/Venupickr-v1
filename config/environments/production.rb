6c596f56debd72ba14f3f53fa6a4f24c45317379
satpai1215 <pai.satyan@gmail.com>
2014-02-20 22:09:23 -0800

    tmp cache dl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Venupickr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.assets.initialize_on_precompile = false
-
 
 
   # Code is not reloaded between requests
@@ -94,4 +92,5 @@ Venupickr::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+   config.assets.initialize_on_precompile = false
 end
