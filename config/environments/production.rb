1f8b737da23a44102a06f99fdacb049bc3c25444
satpai1215 <pai.satyan@gmail.com>
2014-02-20 22:11:29 -0800

    inivialize_on_precimpile to top

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Venupickr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-
+  config.assets.initialize_on_precompile = false
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -92,5 +92,4 @@ Venupickr::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-   config.assets.initialize_on_precompile = false
 end
